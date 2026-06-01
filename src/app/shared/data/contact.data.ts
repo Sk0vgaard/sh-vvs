38cc0c74d9c647039b5afc8a26e64ec74746f6c6
Skovgaard <sk0vgaard@live.dk>
2026-06-01 08:13:35 +0200

    Update contact information in contact.data.ts
    - Changed phoneHref to reflect the new contact number for improved accuracy.

--- src/app/shared/data/contact.data.ts
+++ src/app/shared/data/contact.data.ts
@@ -6,7 +6,7 @@ export interface ContactInfo {
 }
 
 export const CONTACT: ContactInfo = {
-  phoneHref: 'tel:+4571112651',
+  phoneHref: 'tel:+4530144514',
   phoneLabel: '+45 30 14 45 14',
   emailHref: 'mailto:kontakt@shvvs.dk',
   emailLabel: 'kontakt@shvvs.dk',
